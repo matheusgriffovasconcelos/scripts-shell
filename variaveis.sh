7c468074f60f853ecb4adb68b384fa3dd99ab739
Matheus Vasconcelos <matheus.griffo.vasconcelos@gmail.com>
2022-02-08 21:06:46 -0300

    Adicionado arquivo de Variaveis

---
+++ variaveis.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# exemplo de parâmetros de entrada 
+echo "Nome do script : $0" 
+echo "Primeiro parâmetro : $1" 
+echo "Todos os parâmetros : $*" 
+echo "Numero de parametros : $#" 
+echo "Numero deste processo : $$" 
